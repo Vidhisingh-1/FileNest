9bdba0e636423cd9a869e4b5190b7d318a411f12
Vidhi Singh <vidhip.ug22.ec@nitp.ac.in>
2025-07-29 22:17:21 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,24 @@ const app=express();
 const dotenv=require("dotenv");
 dotenv.config();
 const cors=require("cors");
+const connectDB = require("./configs/mongodbconnect");
+const sendmail = require("./service/sendmail");
 const PORT=process.env.PORT;
 
 app.use(cors());
+
+connectDB();
+
+app.use(express.json());
+
+let emailoptions={
+    emailTo:"abc@gmail.com",
+    emailFrom:"cba@gmail.com",
+    link:"abcd",
+    filename:"dcba",size:123
+}
+
+app.get("/send",(req,res)=>{
+    sendmail(emailoptions);
+    res.send("MAIL SENT SUCCESFULLY");
+})
